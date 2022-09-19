e6b332d5a2d28c185cc5bcdfba2ffd89f52ba939
ChrisLv <12737@live.com>
2022-09-19 11:26:49 +0700

    移植CrawlingFLH成DP的形状

---
+++ DynamicPatcher/Projects/Extension.Kratos/TechnoType/CrawlingFLH/CrawlingFLHData.cs
@@ -0,0 +1,59 @@
+using System;
+using DynamicPatcher;
+using PatcherYRpp;
+using Extension.Ext;
+using Extension.INI;
+using Extension.Script;
+using Extension.Utilities;
+
+namespace Extension.Ext
+{
+
+    public class CrawlingFLHData : INIConfig
+    {
+        public CoordStruct PrimaryFireFLH;
+        public CoordStruct PrimaryCrawlingFLH;
+        public CoordStruct ElitePrimaryFireFLH;
+        public CoordStruct ElitePrimaryCrawlingFLH;
+        public CoordStruct SecondaryFireFLH;
+        public CoordStruct SecondaryCrawlingFLH;
+        public CoordStruct EliteSecondaryFireFLH;
+        public CoordStruct EliteSecondaryCrawlingFLH;
+
+        public override void Read(IConfigReader reader)
+        {
+            this.PrimaryFireFLH = reader.Get<CoordStruct>("PrimaryFireFLH", default);
+            this.ElitePrimaryFireFLH = reader.Get<CoordStruct>("ElitePrimaryFireFLH", PrimaryFireFLH);
+
+            CoordStruct primaryCrawlingDefalut = PrimaryFireFLH;
+            primaryCrawlingDefalut.Z = 20;
+            this.PrimaryCrawlingFLH = reader.Get<CoordStruct>("PrimaryCrawlingFLH", primaryCrawlingDefalut);
+            CoordStruct elitePrimaryCrawlingDefalut = ElitePrimaryFireFLH;
+            elitePrimaryCrawlingDefalut.Z = 20;
+            this.ElitePrimaryCrawlingFLH = reader.Get<CoordStruct>("ElitePrimaryCrawlingFLH", elitePrimaryCrawlingDefalut);
+
+            this.SecondaryFireFLH = reader.Get<CoordStruct>("SecondaryFireFLH", default);
+            this.EliteSecondaryFireFLH = reader.Get<CoordStruct>("EliteSecondaryFireFLH", SecondaryFireFLH);
+
+            CoordStruct secondaryCrawlingDefalut = SecondaryFireFLH;
+            secondaryCrawlingDefalut.Z = 20;
+            this.SecondaryCrawlingFLH = reader.Get<CoordStruct>("SecondaryCrawlingFLH", secondaryCrawlingDefalut);
+            CoordStruct eliteSecondaryCrawlingDefalut = EliteSecondaryFireFLH;
+            eliteSecondaryCrawlingDefalut.Z = 20;
+            this.EliteSecondaryCrawlingFLH = reader.Get<CoordStruct>("EliteSecondaryFireFLH", eliteSecondaryCrawlingDefalut);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{{\"PrimaryFireFLH\":{0}, \"PrimaryCrawlingFLH\":{1}, \"ElitePrimaryFireFLH\":{2}, \"ElitePrimaryCrawlingFLH\":{3}, \"SecondaryFireFLH\":{4}, \"SecondaryCrawlingFLH\":{5}, \"EliteSecondaryFireFLH\":{6}, \"EliteSecondaryCrawlingFLH\":{7}}}",
+                PrimaryFireFLH, PrimaryCrawlingFLH,
+                ElitePrimaryFireFLH, ElitePrimaryCrawlingFLH,
+                SecondaryFireFLH, SecondaryCrawlingFLH,
+                EliteSecondaryFireFLH, EliteSecondaryCrawlingFLH
+            );
+        }
+
+    }
+
+
+}
---
+++ DynamicPatcher/Projects/Extension.Kratos/TechnoType/CrawlingFLH/CrawlingFLHScript.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using DynamicPatcher;
+using PatcherYRpp;
+using Extension.Ext;
+using Extension.INI;
+using Extension.Utilities;
+
+namespace Extension.Script
+{
+
+    [Serializable]
+    [GlobalScriptable(typeof(TechnoExt))]
+    [UpdateAfter(typeof(TechnoStatusScript))]
+    public class CrawlingFLHScript : TechnoScriptable
+    {
+        public CrawlingFLHScript(TechnoExt owner) : base(owner) { }
+
+        private IConfigWrapper<CrawlingFLHData> crawlingFLHData;
+
+        public override void Awake()
+        {
+            if (!pTechno.CastIf<InfantryClass>(AbstractType.Infantry, out Pointer<InfantryClass> pInf)
+                || pInf.Ref.Type.Ref.Fearless)
+            {
+                GameObject.RemoveComponent(this);
+                return;
+            }
+            // 获取Image
+            string imgSection = this.section;
+            string image = Ini.GetSection(Ini.RulesDependency, imgSection).Get<string>("Image");
+            if (!string.IsNullOrEmpty(image))
+            {
+                imgSection = image;
+            }
+            crawlingFLHData = Ini.GetConfig<CrawlingFLHData>(Ini.ArtDependency, imgSection);
+        }
+
+        public override void OnUpdate()
+        {
+            if (null != crawlingFLHData)
+            {
+                Pointer<WeaponStruct> primary = pTechno.Ref.GetWeapon(0);
+                Pointer<WeaponStruct> secondary = pTechno.Ref.GetWeapon(1);
+                // Logger.Log("CrawlingFLHData = {0}", crawlingFLHData);
+
+                bool isElite = pTechno.Ref.Veterancy.IsElite();
+                if (pTechno.Convert<InfantryClass>().Ref.Crawling)
+                {
+                    if (isElite)
+                    {
+                        if (null != primary && !primary.IsNull)
+                        {
+                            primary.Ref.FLH = crawlingFLHData.Data.ElitePrimaryCrawlingFLH;
+                        }
+                        if (null != secondary && !secondary.IsNull)
+                        {
+                            secondary.Ref.FLH = crawlingFLHData.Data.EliteSecondaryCrawlingFLH;
+                        }
+                    }
+                    else
+                    {
+                        if (null != primary && !primary.IsNull)
+                        {
+                            primary.Ref.FLH = crawlingFLHData.Data.PrimaryCrawlingFLH;
+                        }
+                        if (null != secondary && !secondary.IsNull)
+                        {
+                            secondary.Ref.FLH = crawlingFLHData.Data.SecondaryCrawlingFLH;
+                        }
+                    }
+                }
+                else
+                {
+                    if (isElite)
+                    {
+                        if (null != primary && !primary.IsNull)
+                        {
+                            primary.Ref.FLH = crawlingFLHData.Data.ElitePrimaryFireFLH;
+                        }
+                        if (null != secondary && !secondary.IsNull)
+                        {
+                            secondary.Ref.FLH = crawlingFLHData.Data.EliteSecondaryFireFLH;
+                        }
+                    }
+                    else
+                    {
+                        if (null != primary && !primary.IsNull)
+                        {
+                            primary.Ref.FLH = crawlingFLHData.Data.PrimaryFireFLH;
+                        }
+                        if (null != secondary && !secondary.IsNull)
+                        {
+                            secondary.Ref.FLH = crawlingFLHData.Data.SecondaryFireFLH;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
