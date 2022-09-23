47898a03516cb79e3d6f1b9d4c21ac9c9e3874e7
ChrisLv <12737@live.com>
2022-09-24 01:03:47 +0700

    移植AutoFireAreaWaapon成DP的形状

---
+++ DynamicPatcher/Projects/Extension.Kratos/TechnoType/AutoFireAreaWeapon/AutoFireAreaWeaponScript.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using DynamicPatcher;
+using PatcherYRpp;
+using PatcherYRpp.Utilities;
+using Extension.Ext;
+using Extension.INI;
+using Extension.Utilities;
+
+namespace Extension.Script
+{
+
+
+    [Serializable]
+    [GlobalScriptable(typeof(TechnoExt))]
+    [UpdateAfter(typeof(TechnoStatusScript))]
+    public class AutoFireAreaWeaponScript : TechnoScriptable
+    {
+        public AutoFireAreaWeaponScript(TechnoExt owner) : base(owner) { }
+
+        private AutoFireAreaWeaponData data => Ini.GetConfig<AutoFireAreaWeaponData>(Ini.RulesDependency, section).Data;
+
+        private TimerStruct reloadTimer;
+        private TimerStruct initialDelayTimer;
+
+        public override void Awake()
+        {
+            Pointer<WeaponStruct> pWeapon = IntPtr.Zero;
+            if (data.WeaponIndex < 0
+                || (pWeapon = pTechno.Ref.GetWeapon(data.WeaponIndex)).IsNull || pWeapon.Ref.WeaponType.IsNull)
+            {
+                GameObject.RemoveComponent(this);
+                return;
+            }
+            initialDelayTimer.Start(data.InitialDelay);
+        }
+
+        public override void OnUpdate()
+        {
+            if (initialDelayTimer.Expired() && reloadTimer.Expired())
+            {
+                Pointer<WeaponStruct> pWeapon = pTechno.Ref.GetWeapon(data.WeaponIndex);
+                // 检查弹药消耗
+                int technoAmmo = pTechno.Ref.Ammo;
+                int weaponAmmo = Ini.GetConfig<WeaponTypeData>(Ini.RulesDependency, pWeapon.Ref.WeaponType.Ref.Base.ID).Data.Ammo;
+                if (technoAmmo >= 0)
+                {
+                    int leftAmmo = technoAmmo - weaponAmmo;
+                    if (data.CheckAmmo || data.UseAmmo)
+                    {
+                        if (leftAmmo < 0)
+                        {
+                            return;
+                        }
+                    }
+                    if (data.UseAmmo)
+                    {
+                        pTechno.Ref.Ammo = leftAmmo;
+                        pTechno.Ref.ReloadNow();
+                    }
+                }
+                reloadTimer.Start(pWeapon.Ref.WeaponType.Ref.ROF);
+                if (data.TargetToGround)
+                {
+                    CoordStruct location = pTechno.Ref.Base.Base.GetCoords();
+                    if (MapClass.Instance.TryGetCellAt(location, out Pointer<CellClass> pCell) && !pCell.IsNull)
+                    {
+                        pTechno.Ref.Fire_IgnoreType(pCell.Convert<AbstractClass>(), data.WeaponIndex);
+                    }
+                }
+                else
+                {
+                    pTechno.Ref.Fire_IgnoreType(pTechno.Convert<AbstractClass>(), data.WeaponIndex);
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ DynamicPatcher/Projects/Extension.Kratos/WeaponType/WeaponTypeData.cs
@@ -0,0 +1,24 @@
+using System;
+using DynamicPatcher;
+using PatcherYRpp;
+using Extension.Ext;
+using Extension.INI;
+using Extension.Script;
+using Extension.Utilities;
+
+namespace Extension.Ext
+{
+
+    public class WeaponTypeData : INIAutoConfig
+    {
+        // YR
+        // Ares
+        public int Ammo = 1;
+        public int LaserThickness = 0;
+        public bool LaserFade = false;
+        public bool IsSupported = false;
+
+    }
+
+
+}
