8e1883f41c910e4ca37e8e6eb5bc454978242709
ChrisLv <12737@live.com>
2022-09-25 11:21:28 +0700

    移植乖巧乘客成DP的形状

---
+++ DynamicPatcher/Projects/Extension.Kratos/TechnoType/Passengers/PassengersData.cs
@@ -0,0 +1,31 @@
+using System;
+using DynamicPatcher;
+using PatcherYRpp;
+using Extension.Ext;
+using Extension.INI;
+using Extension.Script;
+using Extension.Utilities;
+
+namespace Extension.Ext
+{
+
+    public class PassengersData : INIAutoConfig
+    {
+        public bool OpenTopped;
+
+        [INIField(Key = "Passengers.PassiveAcquire")]
+        public bool PassiveAcquire = true;
+
+        [INIField(Key = "Passengers.ForceFire")]
+        public bool ForceFire = false;
+
+        [INIField(Key = "Passengers.MobileFire")]
+        public bool MobileFire = true;
+
+        [INIField(Key = "Passengers.SameFire")]
+        public bool SameFire = true;
+
+    }
+
+
+}
---
+++ DynamicPatcher/Projects/Extension.Kratos/TechnoType/Passengers/PassengersScript.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using DynamicPatcher;
+using PatcherYRpp;
+using PatcherYRpp.Utilities;
+using Extension.Ext;
+using Extension.INI;
+using Extension.Utilities;
+
+namespace Extension.Script
+{
+
+
+    [Serializable]
+    [GlobalScriptable(typeof(TechnoExt))]
+    [UpdateAfter(typeof(TechnoStatusScript))]
+    public class PassengersScript : TechnoScriptable
+    {
+        public PassengersScript(TechnoExt owner) : base(owner) { }
+
+        private PassengersData data => Ini.GetConfig<PassengersData>(Ini.RulesDependency, section).Data;
+
+        public override void OnUpdate()
+        {
+            // check the transporter settings
+            Pointer<TechnoClass> pTransporter = pTechno.Ref.Transporter;
+            if (!pTransporter.IsNull)
+            {
+                if (pTransporter.TryGetComponent<PassengersScript>(out PassengersScript transporter))
+                {
+                    PassengersData data = transporter.data;
+                    if (null != data && data.OpenTopped)
+                    {
+                        if (!data.PassiveAcquire)
+                        {
+                            Mission transporterMission = pTransporter.Convert<ObjectClass>().Ref.GetCurrentMission();
+                            // Mission mission = pTechno.Convert<ObjectClass>().Ref.GetCurrentMission();
+                            if (transporterMission != Mission.Attack)
+                            // if (mission != Mission.Attack && mission != Mission.Sleep)
+                            {
+                                pTechno.Convert<MissionClass>().Ref.QueueMission(Mission.Sleep, true);
+                            }
+                        }
+                        if (data.ForceFire)
+                        {
+                            pTechno.Ref.SetTarget(pTransporter.Ref.Target);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public override void CanFire(Pointer<AbstractClass> pTarget, Pointer<WeaponTypeClass> pWeapon, ref bool ceaseFire)
+        {
+            // check the transporter settings
+            Pointer<TechnoClass> pTransporter = pTechno.Ref.Transporter;
+            if (!pTransporter.IsNull)
+            {
+                if (pTransporter.TryGetComponent<PassengersScript>(out PassengersScript transporter))
+                {
+                    PassengersData data = transporter.data;
+                    if (null != data && data.OpenTopped)
+                    {
+                        Mission transporterMission = pTransporter.Convert<ObjectClass>().Ref.GetCurrentMission();
+                        switch (transporterMission)
+                        {
+                            case Mission.Attack:
+                                ceaseFire = !data.SameFire;
+                                break;
+                            case Mission.Move:
+                            case Mission.AttackMove:
+                                ceaseFire = !data.MobileFire;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
