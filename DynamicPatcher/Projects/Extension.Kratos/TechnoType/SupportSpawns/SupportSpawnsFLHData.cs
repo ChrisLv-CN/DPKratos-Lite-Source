89f5b939db7bf294ae2c8760104109a7b47c96a7
ChrisLv <12737@live.com>
2023-01-31 01:38:54 +0800

    子机支援武器精英的开火坐标复制自普通

--- DynamicPatcher/Projects/Extension.Kratos/TechnoType/SupportSpawns/SupportSpawnsFLHData.cs
+++ DynamicPatcher/Projects/Extension.Kratos/TechnoType/SupportSpawns/SupportSpawnsFLHData.cs
@@ -10,13 +10,31 @@ namespace Extension.Ext
 {
 
     [Serializable]
-    public class SupportSpawnsFLHData : INIAutoConfig
+    public class SupportSpawnsFLHData : INIConfig
     {
         public CoordStruct SupportWeaponFLH = default;
         public CoordStruct EliteSupportWeaponFLH = default;
 
         public CoordStruct SupportWeaponHitFLH = default;
         public CoordStruct EliteSupportWeaponHitFLH = default;
+
+        public SupportSpawnsFLHData()
+        {
+            this.SupportWeaponFLH = default;
+            this.EliteSupportWeaponFLH = default;
+
+            this.SupportWeaponHitFLH = default;
+            this.EliteSupportWeaponHitFLH = default;
+        }
+
+        public override void Read(IConfigReader reader)
+        {
+            this.SupportWeaponFLH = reader.Get("SupportWeaponFLH", this.SupportWeaponFLH);
+            this.EliteSupportWeaponFLH = reader.Get("EliteSupportWeaponFLH", this.SupportWeaponFLH);
+
+            this.SupportWeaponHitFLH = reader.Get("SupportWeaponHitFLH", this.SupportWeaponHitFLH);
+            this.EliteSupportWeaponHitFLH = reader.Get("EliteSupportWeaponHitFLH", this.SupportWeaponHitFLH);
+        }
 
     }
 
